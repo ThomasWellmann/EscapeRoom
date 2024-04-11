5c0918117f3b52df22a0aa4b0bab0421c55066e8
ThomasWellmann <thomadastube@gmail.com>
2024-04-11 11:13:25 +0200

    .

--- Escape Room/Room.cs
+++ Escape Room/Room.cs
@@ -15,7 +15,7 @@ namespace Escape_Room
         {
             room = new char[roomLength, roomHeight];
             
-            for (int x = 0; x < roomLength; x++)
+            for (int x = 0; x < roomLength; x++) 
                 room[x, 0] = '▄';
             
             for (int y = 1; y < roomHeight - 1; y++)
